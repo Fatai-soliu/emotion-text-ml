5e39c45021c4e616165fe00e831c2d5960c30170
Fatai-soliu <fataisoliu.fs@gmail.com>
2026-03-23 17:11:32 +0000

    frontend skeleton added using Flask framework and html

--- app/app/frontend/app.py
+++ app/app/frontend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 
 ##WSGI Application
 
@@ -10,7 +10,7 @@ def welcome():
 
 @app.route("/index")
 def index():
-    return render_template('index.html')
+    return "Welcome to the index page"
 
 if __name__=="__main__":
     app.run(debug=True, port=5001)
