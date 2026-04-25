73d9e29fd78d5c3b163d7ba4c31e27f18b8ad805
Fatai-soliu <fataisoliu.fs@gmail.com>
2026-04-25 16:42:32 +0100

    Added visualisation

--- app/getpost.py
+++ app/getpost.py
@@ -8,13 +8,19 @@ app=Flask(__name__)
 def welcome():
     return "Welcome to the best flask course. This should be an amazing course"
 
-@app.route("/index")
+@app.route("/index", methods=['GET'])
 def index():
     return "Welcome to the index page"
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    if request.method=='POST':
+        pass
+    return render_template('form.html') 
 
 if __name__=="__main__":
     app.run(debug=True, port=5001)
